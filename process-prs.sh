380c0fa3340e2aff8b96d3b3326ae399c0ced79c
Frederic Lepied <frederic.lepied@redhat.com>
2017-11-11 08:58:33 +0000

    handle error cases and use the DLRN_HOME var

--- process-prs.sh
+++ process-prs.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#
+# Copyright (C) 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
 if [ $# != 1 -o ! -r "$1" ]; then
     echo "Usage: $0 <project file>" 1>&2
@@ -22,6 +36,11 @@ if [ -z "$SERVER_URL" ]; then
     exit 1
 fi
 
+if [ -z "$DLRN_HOME" ]; then
+    echo "DLRN_HOME unset in $FILE" 1>&2
+    exit 1
+fi
+
 if [ -z "$GITHUB_ACCOUNT" ]; then
     GITHUB_ACCOUNT=$PROJECT
 fi
@@ -36,16 +55,20 @@ done | sort -rn | while read times pullreq sha1; do
     (
 	echo "========================================"
 	date
-	$dir/github_vote.py $GITHUB_ACCOUNT/$PROJECT $sha1 pending "$CI_CONTEXT" "$CI_DETAIL pending"
+	$dir/github_vote.py $GITHUB_ACCOUNT/$PROJECT $sha1 pending "$CI_CONTEXT" "$CI_DETAIL [pending]"
 	mv $PROJECT/todo/$pullreq $PROJECT/done/$pullreq
-	cd $HOME/DLRN
+	cd $DLRN_HOME
 	if $dir/extract-pr.sh $pullreq $FILE $sha1; then
 	    ln -f $dir/$PROJECT/done/$pullreq $dir/$PROJECT/success/$pullreq
-	    $dir/github_vote.py $GITHUB_ACCOUNT/$PROJECT $sha1 success "$CI_CONTEXT" "$CI_DETAIL succeeded" $SERVER_URL/$PROJECT-pr/${sha1:0:2}/${sha1:2:2}/${sha1}_dev/
+	    $dir/github_vote.py $GITHUB_ACCOUNT/$PROJECT $sha1 success "$CI_CONTEXT" "$CI_DETAIL [succeeded]" $SERVER_URL/$PROJECT-pr/${sha1:0:2}/${sha1:2:2}/${sha1}_dev/
 	else
 	    ln -f $dir/$PROJECT/done/$pullreq $dir/$PROJECT/failure/$pullreq
-	    $dir/github_vote.py $GITHUB_ACCOUNT/$PROJECT $sha1 failure "$CI_CONTEXT" "$CI_DETAIL failed" $SERVER_URL/$PROJECT-pr/${sha1:0:2}/${sha1:2:2}/${sha1}_dev/build.log.gz
-	fi
+            if [ -f $PROJECT-pr-data/repos/${sha1:0:2}/${sha1:2:2}/${sha1}_dev/build.log.gz ]; then
+                $dir/github_vote.py $GITHUB_ACCOUNT/$PROJECT $sha1 failure "$CI_CONTEXT" "$CI_DETAIL [failed]" $SERVER_URL/$PROJECT-pr/${sha1:0:2}/${sha1:2:2}/${sha1}_dev/build.log.gz
+            else
+                $dir/github_vote.py $GITHUB_ACCOUNT/$PROJECT $sha1 error "$CI_CONTEXT" "$CI_DETAIL [internal error]" $SERVER_URL/$PROJECT-pr/${sha1:0:2}/${sha1:2:2}/${sha1}_dev/rpmbuild.log
+            fi
+        fi
     )
     [ -f stop ] && exit
 done
